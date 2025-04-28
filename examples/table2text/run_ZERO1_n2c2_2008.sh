c0132c385b7c83c34d7c8071665e49e51d2d64d3
Viktor Schlegel <v.schlegel@imperial.ac.uk>
2025-04-28 07:26:07 +0000

    uncomment commented stuff

--- examples/table2text/run_ZERO1_n2c2_2008.sh
+++ examples/table2text/run_ZERO1_n2c2_2008.sh
@@ -52,31 +52,31 @@ max_seq_len=3072
 learning_rate="1e-4"
 mkdir -p result/
 num_train_epochs=15
-# for target_epsilon in 0.5 1 2 4; do # 0.5 1 2 4; do
+for target_epsilon in 0.5 1 2 4; do # 0.5 1 2 4; do
 
-#   output_dir="result/n2c2_2008_$target_epsilon"
-#   deepspeed table2text/run_language_modeling.py --deepspeed_config ${deepspeed_config} \
-#     --output_dir ${output_dir} --overwrite_output_dir \
-#     --task_mode ${task_mode} \
-#     --model_name_or_path ${model_name_or_path} \
-#     --tokenizer_name ${model_name_or_path} \
-#     --do_train \
-#     --save_steps 50 --save_total_limit 2 --save_at_last yes \
-#     --logging_dir ${output_dir} --logging_steps 10 \
-#     --seed 0 \
-#     --dataloader_num_workers 2 \
-#     --eval_steps -1 --eval_epochs 999 --max_eval_batches 100 --evaluation_strategy epoch --evaluate_before_training "no" \
-#     --evaluate_during_training "no" --per_device_eval_batch_size 10 \
-#     --max_generations 9223372036854775807 --max_generations_train 10 --max_generations_valid 9223372036854775807 \
-#     --max_train_examples 9223372036854775807 --max_valid_examples 9223372036854775807 --max_eval_examples 9223372036854775807 \
-#     --data_folder ${data_dir} --max_seq_len ${max_seq_len} --format_mode cat \
-#     --per_example_max_grad_norm 0.1 --target_delta ${target_delta} --target_epsilon ${target_epsilon} \
-#     --learning_rate ${learning_rate} --lr_decay "no" --num_train_epochs ${num_train_epochs} --per_device_train_batch_size ${physical_batch_size} --logical_batch_size ${batch_size}\
-#     --attention_only ${attention_only} --bias_only ${bias_only} --static_lm_head ${static_lm_head} --static_embedding ${static_embedding} \
-#     --non_private ${non_private} \
-#     --clipping_mode "${clipping_mode}" --clipping_fn "${clipping_fn}" --clipping_style "${clipping_style}" \
-#     --tf32 True --gradient_checkpointing
-# done
+  output_dir="result/n2c2_2008_$target_epsilon"
+  deepspeed table2text/run_language_modeling.py --deepspeed_config ${deepspeed_config} \
+    --output_dir ${output_dir} --overwrite_output_dir \
+    --task_mode ${task_mode} \
+    --model_name_or_path ${model_name_or_path} \
+    --tokenizer_name ${model_name_or_path} \
+    --do_train \
+    --save_steps 50 --save_total_limit 2 --save_at_last yes \
+    --logging_dir ${output_dir} --logging_steps 10 \
+    --seed 0 \
+    --dataloader_num_workers 2 \
+    --eval_steps -1 --eval_epochs 999 --max_eval_batches 100 --evaluation_strategy epoch --evaluate_before_training "no" \
+    --evaluate_during_training "no" --per_device_eval_batch_size 10 \
+    --max_generations 9223372036854775807 --max_generations_train 10 --max_generations_valid 9223372036854775807 \
+    --max_train_examples 9223372036854775807 --max_valid_examples 9223372036854775807 --max_eval_examples 9223372036854775807 \
+    --data_folder ${data_dir} --max_seq_len ${max_seq_len} --format_mode cat \
+    --per_example_max_grad_norm 0.1 --target_delta ${target_delta} --target_epsilon ${target_epsilon} \
+    --learning_rate ${learning_rate} --lr_decay "no" --num_train_epochs ${num_train_epochs} --per_device_train_batch_size ${physical_batch_size} --logical_batch_size ${batch_size}\
+    --attention_only ${attention_only} --bias_only ${bias_only} --static_lm_head ${static_lm_head} --static_embedding ${static_embedding} \
+    --non_private ${non_private} \
+    --clipping_mode "${clipping_mode}" --clipping_fn "${clipping_fn}" --clipping_style "${clipping_style}" \
+    --tf32 True --gradient_checkpointing
+done
 
 
 output_dir="result/n2c2_2008_nodp"
